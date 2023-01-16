63bad66e4290ca8b3ea8bcc95e1c9fc9619e1a62
Ahmed Ayaz <ahmedayaz.business@gmail.com>
2023-01-16 21:24:25 +0500

    Completed Counter App Using Class Component

--- step-01-counter-app-usingclasscomponent/src/App.js
+++ step-01-counter-app-usingclasscomponent/src/App.js
@@ -2,13 +2,46 @@ import React , {Component} from 'react';
 import Navbar from './component/master-components/Navbar';
 import Counter from './component/Counter';
 
+
+let bag = {
+    border : "2px solid black" ,
+    display : "flex" ,
+    padding : "20px"
+}
+
+
+let button = {
+    padding : "20px 50px" ,
+    backgroundColor : "dodgerblue" ,
+    border : "none" ,
+    outline : "none" , 
+    margin : "10px"
+}
+
 class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            counter1 : true ,
+            counter2 : false
+        }
+    }
+
+    showAnotherCounter = () =>{
+        this.setState({...this.state , counter1 : !this.state.counter1 , counter2 : !this.state.counter2})
+    }
+
     render(){
         return(
             <>
                 <Navbar />
-                <Counter appNo = "1" start = {15}/>
-                <Counter appNo = "2" start = {35}/>
+                <button style={button} onClick={this.showAnotherCounter}>{
+                    this.state.counter1 ? 'Show Counter 2' : 'Show Counter 1'
+                }</button>
+                <div className='bag' style={bag}>
+                    {this.state.counter1 && <Counter appNo = {1}/>}
+                    {this.state.counter2 && <Counter appNo = {2}/>}
+                </div>
             </>
         )
     }
--- step-01-counter-app-usingclasscomponent/src/component/Counter.js
+++ step-01-counter-app-usingclasscomponent/src/component/Counter.js
@@ -1,39 +1,52 @@
 import React, { Component } from "react";
 
+
+let container = {
+    width : "500px",
+    border : "2px solid black" ,
+    backgroundColor : "rgb(245, 255, 151)" ,
+    display : "flex",
+    flexDirection : "column" ,
+    gap : "30px" , 
+    height : "200px" ,
+    margin : "0px auto" ,
+    textAlign : "center"
+}
+
 class Counter extends Component{
-    constructor(props){
+   constructor(props){
         super(props);
         this.state = {
-            counter : this.props.start
+            counter : 1
         }
-    }
+   }
 
-    increment = () =>{
+    increment = () => {
+    this.setState({
+        ...this.state ,
+        counter : this.state.counter + 1
+    })}
+
+    decrement = () => {
         this.setState({
             ...this.state ,
-            counter : this.state.counter + 1
-        })
-    }
-    
-    decrement = () =>{
-        this.setState({
-            ...this.state ,
-            counter : this.state.counter -1
-        })
-    }
+            counter : this.state.counter - 1
+        })}
 
     render(){
         return (
-            <>
-                <h1 className="heading">Counter App No. {this.props.appNo}</h1>
+            <div className="container" style={container}>
+                <h1 className="heading">Counter App No - {this.props.appNo}</h1>
                 <span>{this.state.counter}</span>
                 <div>
-                    <button onClick={this.increment}>+</button>
-                    <button onClick={this.decrement}>-</button>
+                    <button className = {`button${this.props.appNo}`} onClick={this.increment}>+</button>
+                    <button className = {`button${this.props.appNo}`} onClick={this.decrement}>-</button>
                 </div>
-            </>
+            </div>
         )
     }
 }
 
 export default Counter;
+
+
